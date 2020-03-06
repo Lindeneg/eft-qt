31221a7bff31a4ca4fafd7bf32515e4a39169305
wh473v3rw0rk5 <christianlindeneg@gmail.com>
2020-03-06 19:43:35 +0100

    include two item model views

---
+++ backend/src/items/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+
+from items.models import Item
+from .serializers import ItemSerializer
+
+
+class ItemListView(ListAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+
+class ItemDetailView(RetrieveAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
