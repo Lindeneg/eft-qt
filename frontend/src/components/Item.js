47367cd71fd23f4221b8e198f13772f3ced0ef4b
wh473v3rw0rk5 <christianlindeneg@gmail.com>
2020-03-07 04:35:50 +0100

    item description

--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -1,32 +1,53 @@
 import React from 'react';
-import { List, Avatar } from 'antd';
+import { List } from 'antd';
 
-const data = [
-  {
-    title: 'Ant Design Title 1',
-  },
-  {
-    title: 'Ant Design Title 2',
-  },
-  {
-    title: 'Ant Design Title 3',
-  },
-  {
-    title: 'Ant Design Title 4',
-  },
-];
+
+const ItemDescription = (data) => {
+  let mData = data.replace(/'/g, '"');
+  mData = mData.replace(/True/g, true);
+  mData = mData.replace(/False/g, false);
+  const newData = JSON.parse(mData);
+  return `
+  Appears in ${newData.quests.length} ${Plural(newData.quests.length, "Quest")} | 
+  Used ${newData.hideout.length} ${Plural(newData.hideout.length, "time")} in Hideout 
+  ${UsedWhereString(newData.barter_item, newData.crafting_item)}`;
+}
+
+
+const UsedWhereString = (barter, crafting) => {
+  if (barter === true && crafting === true) {
+    return "| Barter Item | Crafting Item";
+  }
+  if (barter === true && crafting === false) {
+    return "| Barter Item";
+  }
+  if (barter === false && crafting === true) {
+    return "| Crafting Item";
+  }
+  if (barter === false && crafting === false) {
+    return "";
+  }
+}
+
+
+const Plural = (len, s) => {
+  if (len === 1) {
+    return s
+  } else {
+    return s + "s"
+  }
+}
 
 const Item = (props) => {
     return (
         <List
         itemLayout="horizontal"
-        dataSource={data}
+        dataSource={props.data}
         renderItem={item => (
             <List.Item>
             <List.Item.Meta
-                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                title={<a href="https://ant.design">{item.title}</a>}
-                description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                title={<a href="https://ant.design">{item.name}</a>}
+                description={ItemDescription(item.notes)}
             />
             </List.Item>
         )}
