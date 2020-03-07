3a318976fe044026edcd2d106373e0da24946e14
wh473v3rw0rk5 <christianlindeneg@gmail.com>
2020-03-07 15:24:23 +0100

    cleaning

--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -1,41 +1,16 @@
 import React from 'react';
 import { List } from 'antd';
 
+import Replacer from '../Tools/Replacer';
+import BCString from '../Tools/BCString';
+import Plural from '../Tools/Plural';
 
 const ItemDescription = (data) => {
-  let mData = data.replace(/'/g, '"');
-  mData = mData.replace(/True/g, true);
-  mData = mData.replace(/False/g, false);
-  const newData = JSON.parse(mData);
+  const newData = Replacer(data);
   return `
   Appears in ${newData.quests.length} ${Plural(newData.quests.length, "Quest")} | 
   Used ${newData.hideout.length} ${Plural(newData.hideout.length, "time")} in Hideout 
-  ${UsedWhereString(newData.barter_item, newData.crafting_item)}`;
-}
-
-
-const UsedWhereString = (barter, crafting) => {
-  if (barter === true && crafting === true) {
-    return "| Barter Item | Crafting Item";
-  }
-  if (barter === true && crafting === false) {
-    return "| Barter Item";
-  }
-  if (barter === false && crafting === true) {
-    return "| Crafting Item";
-  }
-  if (barter === false && crafting === false) {
-    return "";
-  }
-}
-
-
-const Plural = (len, s) => {
-  if (len === 1) {
-    return s
-  } else {
-    return s + "s"
-  }
+  ${BCString(newData.barter_item, newData.crafting_item)}`;
 }
 
 const Item = (props) => {
@@ -46,7 +21,7 @@ const Item = (props) => {
         renderItem={item => (
             <List.Item>
             <List.Item.Meta
-                title={<a href="https://ant.design">{item.name}</a>}
+                title={<a href="/">{item.name}</a>}
                 description={ItemDescription(item.notes)}
             />
             </List.Item>
