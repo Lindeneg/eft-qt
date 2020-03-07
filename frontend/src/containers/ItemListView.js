7d8dc5a0ef406b988f89f11aff1ccae7f588ef90
wh473v3rw0rk5 <christianlindeneg@gmail.com>
2020-03-07 02:57:29 +0100

    create skeleton Item component

---
+++ frontend/src/containers/ItemListView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Item from '../components/Item'
+
+
+class ItemList extends React.Component {
+    render() {
+        return (
+            <Item />
+        );
+    }
+}
+
+export default ItemList;
