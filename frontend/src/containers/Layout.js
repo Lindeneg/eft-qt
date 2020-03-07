939adf171a78ca80f7f0e41cb4f13ad2cb74b126
wh473v3rw0rk5 <christianlindeneg@gmail.com>
2020-03-07 16:06:54 +0100

    layout search template

--- frontend/src/containers/Layout.js
+++ frontend/src/containers/Layout.js
@@ -1,27 +1,33 @@
 import React from 'react';
-import { Layout, Breadcrumb } from 'antd';
+import { Link } from 'react-router-dom';
+import { Layout, Breadcrumb, Input  } from 'antd';
 import './Layout.css';
 
 const { Header, Content, Footer } = Layout;
+const { Search } = Input;
 
 
 const DefaultLayout = (props) => {
     return (
         <Layout className="layout">
         <Header>
-            {/* Search Bar */}
+        <div>
+            <Search
+            placeholder="(Currently Inactive)"
+            onSearch={value => console.log(props.dataSource)}
+            style={{ width: 200 }}
+            />
+        </div>
         </Header>
         <Content style={{ padding: '0 50px' }}>
             <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
+            <Breadcrumb.Item><Link to="/">Home</Link></Breadcrumb.Item>
             </Breadcrumb>
             <div className="site-layout-content">
                 {props.children}
             </div>
         </Content>
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        <Footer style={{ textAlign: 'center' }}>Made by Christian Lindeneg</Footer>
         </Layout>
     );
 }
