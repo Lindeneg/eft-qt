6f68f61ee26a7fc5cade51dee839e0dbd9676851
wh473v3rw0rk5 <christianlindeneg@gmail.com>
2020-03-09 16:37:09 +0100

    moving constant.py to scraper.py - no reason to have constants in seperate file as only one file uses those constants

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -10,19 +10,31 @@ The data comes from the unofficial Tarkov gamepedia available under the CC BY-NC
 https://escapefromtarkov.gamepedia.com
 """
 
-from typing import Union, MutableSequence, Sequence, Tuple, List, MutableMapping, Optional, Dict, Any
+from os import path
+from string import ascii_letters, digits
 from sys import exit as sExit
 from time import time
+from typing import Union, MutableSequence, Sequence, Tuple, List, MutableMapping, Optional, Dict, Any, Final, Callable, Mapping
 
 from lxml import html
 from lxml.html import HtmlElement
 from requests import get, post, Response, RequestException, ConnectionError
 from bs4 import BeautifulSoup
 
-try:
-    from .constants import Constants as const
-except ImportError:
-    from constants import Constants as const
+
+# Constant variables and types
+
+NOTE_TYPE = MutableMapping[str, Any]
+ITEM_TYPE = MutableMapping[str, Union[str, NOTE_TYPE, Mapping[str, Union[str, None, bool]]]]
+XPATH: Callable[[str], str] = lambda n: f'/html/body/div[2]/div[3]/div[1]/div[4]/div[4]/div/table[1]/tbody/tr[{n}]'
+WIKI_URL: Final[str] = 'https://escapefromtarkov.gamepedia.com'
+LOOT_PARAMETER: Final[str] = 'Loot'
+LEGAL_SYMBOLS: Final[str] = ascii_letters + digits + ' '
+BARTER: Final[str] = 'barter_item'
+CRAFTING: Final[str] = 'crafting_item'
+HIDEOUT: Final[str] = 'hideout'
+QUESTS: Final[str] = 'quests'
+QUEST: Final[str] = 'quest '
 
 
 class Scraper:
@@ -30,13 +42,13 @@ class Scraper:
     Scraping and posting class
     """
     def __init__(self) -> None:
-        self.items: Sequence[const.ITEM_TYPE] = []
+        self.items: Sequence[ITEM_TYPE] = []
 
     def update_items(self) -> None:
         start: float = time()
         content: HtmlElement = GetContent()
         self.items = SortContent(content)
-        print(f"Updated {len(self.items)} items in {abs(start-time())} seconds")
+        print(f"\nFetched {len(self.items)} items in {abs(start-time())} seconds")
     
     def update_backend(
         self,
@@ -72,22 +84,22 @@ def GetContent() -> HtmlElement:
     """
     Get html content from the Tarkov wiki Loot page
     """
-    req: Response = get(f"{const.WIKI_URL}/{const.LOOT_PARAMETER}")
+    req: Response = get(f"{WIKI_URL}/{LOOT_PARAMETER}")
     if req.status_code == 200:
         return html.fromstring(req.content)
     print(f"Failed to establish connection with error code {req.status_code}\nContent\n{req.content}") #type: ignore[str-bytes-safe]
     sExit()
 
 
-def SortContent(HTMLContent: HtmlElement) -> MutableSequence[const.ITEM_TYPE]:
+def SortContent(HTMLContent: HtmlElement) -> MutableSequence[ITEM_TYPE]:
     """
     Sorts the html content into an item type
     """
-    items: MutableSequence[const.ITEM_TYPE] = []
+    items: MutableSequence[ITEM_TYPE] = []
     # start from second n in xpath - first n is the table header
     n: int = 2
     while True:
-        data: Union[None, MutableSequence[bytes]] = [html.tostring(item) for item in HTMLContent.xpath(const.XPATH(str(n)))]
+        data: Union[None, MutableSequence[bytes]] = [html.tostring(item) for item in HTMLContent.xpath(XPATH(str(n)))]
         if data and len(data) >= 1:
             soup: BeautifulSoup = BeautifulSoup(data[0], 'lxml')
             itemData: MutableSequence[str] = RemoveEmptyItems(soup.text.split("\n"))
@@ -97,7 +109,7 @@ def SortContent(HTMLContent: HtmlElement) -> MutableSequence[const.ITEM_TYPE]:
             if imgPath is not False:
                 items.append({
                     'name': itemData[0],
-                    'url': f'{const.WIKI_URL}/{itemData[0].replace(" ", "_")}',
+                    'url': f'{WIKI_URL}/{itemData[0].replace(" ", "_")}',
                     'item_type': itemData[1],
                     'notes': SortNotes(notes),
                     'img_info': {
@@ -125,43 +137,43 @@ def CheckForAdditionelSplits(notes: MutableSequence[str]) -> MutableSequence[str
     sortedList = []
     for note in notes:
         for i in range(len(note)-1):
-            if note[i] in const.LEGAL_SYMBOLS and note[i+1] in const.LEGAL_SYMBOLS and note[i].islower() and note[i+1].isupper():
+            if note[i] in LEGAL_SYMBOLS and note[i+1] in LEGAL_SYMBOLS and note[i].islower() and note[i+1].isupper():
                 sortedList.extend([note[:i+1], note[i+1:]])
     if not sortedList:
         return notes
     return CheckForAdditionelSplits(sortedList)
 
 
-def SortNotes(notes: MutableSequence[str]) -> const.NOTE_TYPE:
+def SortNotes(notes: MutableSequence[str]) -> NOTE_TYPE:
     """
     Sorts a sequence of strings descriping the item into an note type
     """
-    sortedNotes: const.NOTE_TYPE = {
-        const.BARTER: False,
-        const.CRAFTING: False,
-        const.QUESTS: {},
-        const.HIDEOUT: {}
+    sortedNotes: NOTE_TYPE = {
+        BARTER: False,
+        CRAFTING: False,
+        QUESTS: {},
+        HIDEOUT: {}
     }
     note: str
     for note in notes:
-        if note.lower() == const.BARTER.replace('_', ' '):
-            sortedNotes[const.BARTER] = True
-        if note.lower() == const.CRAFTING.replace('_', ' '):
-            sortedNotes[const.CRAFTING] = True
-        if note.lower() == const.QUESTS:
+        if note.lower() == BARTER.replace('_', ' '):
+            sortedNotes[BARTER] = True
+        if note.lower() == CRAFTING.replace('_', ' '):
+            sortedNotes[CRAFTING] = True
+        if note.lower() == QUESTS:
             i: int = notes.index(note) + 1
             while i < len(notes):
-                if notes[i].lower() == const.HIDEOUT:
+                if notes[i].lower() == HIDEOUT:
                     break
-                sortedNotes[const.QUESTS][str(i)] = notes[i]
+                sortedNotes[QUESTS][str(i)] = notes[i]
                 i += 1
-        if note.lower() == const.HIDEOUT:
+        if note.lower() == HIDEOUT:
             j: int = notes.index(note) + 1
             while j < len(notes):
-                sortedNotes[const.HIDEOUT][str(j)] = notes[j]
+                sortedNotes[HIDEOUT][str(j)] = notes[j]
                 j += 1
-    sortedNotes[const.QUESTS] = SortQH(sortedNotes[const.QUESTS])
-    sortedNotes[const.HIDEOUT] = SortQH(sortedNotes[const.HIDEOUT])
+    sortedNotes[QUESTS] = SortQH(sortedNotes[QUESTS])
+    sortedNotes[HIDEOUT] = SortQH(sortedNotes[HIDEOUT])
     return sortedNotes
 
 
@@ -172,8 +184,8 @@ def SortQH(items: MutableMapping[int, str]) -> MutableMapping[int, str]:
     mItems: MutableMapping[int, str] = {}
     for k, v in items.items():
         bV = v.upper()
-        if bV != const.BARTER.upper() and bV != const.BARTER.replace("_", " ").upper() \
-            and bV != const.CRAFTING.upper() and bV != const.CRAFTING.replace("_", " ").upper():
+        if bV != BARTER.upper() and bV != BARTER.replace("_", " ").upper() \
+            and bV != CRAFTING.upper() and bV != CRAFTING.replace("_", " ").upper():
             v = (v.replace('"', "")).replace("'", "")
             mItems[k] = v
     return mItems
